bc4152afc9aa4cd5578b59af160965eb82c94e57
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-03-12 11:12:20 +0500

    update file object.ts

--- object.ts
+++ object.ts
@@ -4,7 +4,11 @@ let  fruit = {
     test: "sweet",
     howMuch: 25,
     isseasonal: true,
-    kinds: ['desi', 'chunsa', 'langra'],
+    kinds: ['desi', 'chunsa', 'langra', 'beganphali', 'totaPari', 'lalBadshah', 'sunehri'],
     isAvailable: true,
 };
 
+console.log(fruit); //print full object
+console.log(fruit.kinds); // print full array
+console.log(fruit.kinds[4]) // totaPari (print spacific elemnt )
+console.log(fruit.test); //sweet (print a key value of the object)
